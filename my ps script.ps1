84d43b4460f875baadd5b93a9089460e1af94769
PrasadDedhe2 <142909228+PrasadDedhe2@users.noreply.github.com>
2023-08-23 23:51:40 +0530

    Delete my ps script.ps1

--- my ps script.ps1
+++
@@ -1,4 +0,0 @@
-﻿$date = Get-Date
-$date1 = $date.AddMinutes(-30)
-$events = Get-WinEvent -LogName *PowerShell* | Where-Object { $_.TimeCreated -ge $date1 }
-$events | Select-Object TimeCreated, Id, Message
